1fdf347b365eda6c85a4c7ae5ee5a68490ad857f
Avigail <avi605205@gmail.com>
2026-05-13 21:10:05 +0300

    m

--- client/src/services/recipesService.ts
+++ client/src/services/recipesService.ts
@@ -1,10 +1,11 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getRecipes = async (category?: string) => {
+  console.log("GET recipes hit");
   const url = category
     ? `${BASE_URL}/recipes?category=${category}`
     : `${BASE_URL}/recipes`;
-
+console.log("before db call");
   const res = await fetch(url);
   if (!res.ok) {
   throw new Error("API error");
